76d2f7062741efd52cba920068586f020a2ea5a6
Jaykit <92615198+Rewind789@users.noreply.github.com>
2021-11-12 10:05:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("Penguin Go Has Successfully Opened! Opening Recent File When You Left")
+function newPage() {
+  var temp = document.getElementsByTagName("template")[0];
+  var clon = temp.content.cloneNode(true);
+  document.body.appendChild(clon);
+  window.scrollTo(0,document.body.scrollHeight);
+}
+function toTop() {
+  window.scrollTo(0,0);
+}
+function align() {
+  document.getElementById("title").style.textAlign = "left";
+}
+area.value = localStorage.getItem('area');
+area.oninput = () => {
+  localStorage.setItem('area', area.value)
+};
